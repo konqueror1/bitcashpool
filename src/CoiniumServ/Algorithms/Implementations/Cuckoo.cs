fa0662d46a4327d507d6af6c4f7611fc16aba8ab
WillyTheCat <39898519+WillyTheCat@users.noreply.github.com>
2018-09-13 08:18:38 +0200

    Add files via upload

--- src/CoiniumServ/Algorithms/Implementations/Cuckoo.cs
+++ src/CoiniumServ/Algorithms/Implementations/Cuckoo.cs
@@ -5,6 +5,7 @@
 //     CoiniumServ - Crypto Currency Mining Pool Server Software
 //
 //     Copyright (C) 2017 - 2018 The Merit Foundation
+//     Copyright (C) 2018 The BitCash Developers
 //
 //     Permission is hereby granted, free of charge, to any person obtaining a copy
 //     of this software and associated documentation files (the "Software"), to deal
